14855504bec90896198f4f9965b32790531de47c
Nacho García <lupus.zgz@gmail.com>
2018-10-02 07:56:54 +0200

    App shows random phrases

--- cypress/integration/showsRandomPhrases.test.ts
+++ cypress/integration/showsRandomPhrases.test.ts
@@ -1,5 +1,17 @@
 describe('App', function () {
-    it('loads', function () {
+    beforeEach(function () {
+        cy.server();
+        cy.fixture('RandomPhrasesResponse').as('randomPhrases');
+        cy.route('GET', 'https://api.icndb.com/jokes/random/**', '@randomPhrases').as('getRandomPhrases');
+    });
+
+    it('shows random phrases', function () {
         cy.visit('/');
+
+        cy.wait(['@getRandomPhrases']);
+
+        this.randomPhrases.value.forEach((phrase, index) => {
+            cy.get('.phrase').eq(index).should('contain', phrase.joke)
+        })
     })
 });
